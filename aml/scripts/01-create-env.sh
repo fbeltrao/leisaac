f885e1077cb810253a73d0750c6189fabee5c230
Francisco Beltrao <francisco.beltrao@microsoft.com>
2025-12-10 18:43:44 +0000

    Add Dockerfile and scripts for Azure ML environment setup with Nvidia Isaac Lab

---
+++ aml/scripts/01-create-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e # Exit on error
+
+az ml environment create --name isaaclab-azcli-docker --build-context ./aml/docker/. --dockerfile-path ./aml/docker/Dockerfile --tags "git_hash=$(git rev-parse HEAD)" --description "Nvidia Isaac Lab with Azure CLI and Docker CLI" --version 2.3.1
